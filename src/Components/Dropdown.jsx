e3a066c3086777e9cf9dd58b353d86e191f56f11
syeda Faiza <syedafaiza2610@gmail.com>
2024-04-18 23:42:42 +0500

    location work in process

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -14,7 +14,7 @@ import { ClickAwayListener } from '@mui/base/ClickAwayListener';
 
 
 
-function Dropdown() {
+function Dropdown({data}) {
     const [isopenselect , setisopenselect] = useState(false)
     const [selected , setselected] = useState(0)
     const [selecteditem , setselecteditem] = useState('All Categories')
@@ -37,17 +37,14 @@ function Dropdown() {
                  <input type="text"placeholder='Search here' />
              </div>
              <ul className='result'>
+                {
+                    data.map((item , index)=>{
+                        return(
+                            <li onClick={() => closeSelect(index ,item)} className={`${selected === index ? 'active' : " "}`}>{item}</li>
+                    )
+                    })
+                }
                
-                 <li onClick={() => closeSelect(0 , "All Categories")} className={`${selected ===0 ? 'active' : " "}`}>All Categories</li>
-                 <li onClick={() => closeSelect(1 , "Clothing")} className={`${selected ===1 ? 'active' : " "}`}> <GiLargeDress className='icn'/><span></span> Clothing</li>
-                 <li onClick={() => closeSelect(2 , "Beauty")} className={`${selected ===2 ? 'active' : " "}`}><GiLipstick className='icn'/><span></span> Beauty</li>
-                 <li onClick={() => closeSelect(3 , "Foot Wear")} className={`${selected ===3 ? 'active' : " "}`}><GiSonicShoes className='icn'/><span></span> Foot Wear</li>
-                 <li onClick={() => closeSelect(4 , "Baking Items")} className={`${selected ===4 ? 'active' : " "}`}><MdCake className='icn'/><span></span> Baking Items</li>
-                 <li onClick={() => closeSelect(5 , "Bags")} className={`${selected ===5 ? 'active' : " "}`}><FaBagShopping className='icn'/><span></span> Bags</li>
-                 <li onClick={() => closeSelect(6 , "Pets")} className={`${selected ===6 ? 'active' : " "}`}> <FaDog className='icn'/><span></span> Pets</li>
-                 <li onClick={() => closeSelect(7 , "Abaya's")} className={`${selected ===7 ? 'active' : " "}`}><MdOutlineGirl className='icn'/><span></span> Abaya's</li>
-                 <li onClick={() => closeSelect(8 , "Jwellery")} className={`${selected ===8 ? 'active' : " "}`}><GiDiamondRing className='icn'/><span></span> Jwellery</li>
-                 <li onClick={() => closeSelect(9 , "Hijabs")} className={`${selected ===9 ? 'active' : " "}`}> <GiNinjaMask className='icn'/><span></span> Hijabs</li>
              </ul>
 
          </div>
