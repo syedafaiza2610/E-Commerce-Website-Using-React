34c58e41f61d0212326de09a9b89919c5aea7cef
syeda Faiza <syedafaiza2610@gmail.com>
2024-04-20 19:02:36 +0500

    drop down done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,50 +5,117 @@ import { FiGrid } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 import Home from '../Pages/Home';
 import { MdOutlineHeadphones } from "react-icons/md";
+import Mega from'../Assets/mega.png'
 function Navbar() {
     return (
         <div className='nav d-flex align-items-center'>
             <div className="container-fluid">
-                <div className="row">
+                <div className="row position-relative">
                     <div className="col-sm-3 part1 d-flex align-items-center">
                         <Button className='btn' ><FiGrid size={20} /> <span className='head'>Browse All Categories</span> <IoIosArrowDown /></Button>
 
 
                     </div>
-                    <div className="col-sm-7 part2">
+                    <div className="col-sm-7 part2 position-static">
                         <nav>
                             <ul className=' list list-inline mb-0'>
                                 <li className='list-inline-item'>
-                                    <Button><Link  to="/home">Home</Link></Button>
+                                    <Button><Link to="/home">Home</Link></Button>
                                 </li>
                                 <li className='list-inline-item'>
-                                    <Button><Link  to="/about">About</Link></Button>
+                                    <Button><Link to="/about">About</Link></Button>
                                 </li>
                                 <li className='list-inline-item'>
-                                    <Button><Link  to="/shop">Shop</Link></Button>
+                                    <Button><Link to="/shop">Shop</Link></Button>
+                                    <div className="dropdown_menu">
+                                        <ul>
+                                            <li><Button><Link to="/about">About us</Link></Button></li>
+                                            <li><Button><Link to="/contact">Contact</Link></Button></li>
+                                            <li><Button><Link to="/about">My Account</Link></Button></li>
+                                            <li><Button><Link to="/login">Login</Link></Button></li>
+                                            <li><Button><Link to="/register">Register</Link></Button></li>
+                                            <li><Button><Link to="">Forgot Password</Link></Button></li>
+                                            <li><Button><Link to="">Reset Password</Link></Button></li>
+                                            <li><Button><Link to="">Purchase Guide</Link></Button></li>
+                                            <li><Button><Link to="">Privacy Policy</Link></Button></li>
+                                            <li><Button><Link to="">Term & Services</Link></Button></li>
+                                            <li><Button><Link to="*">404 Page</Link></Button></li>
+                                        
+                                        </ul>
+                                    </div>
                                 </li>
                                 <li className='list-inline-item'>
-                                    <Button><Link  to="/listing">Vendors <IoIosArrowDown /></Link></Button>
+                                    <Button><Link to="/listing">Vendors</Link></Button>
+                                    <div className="dropdown_menu">
+                                        <ul>
+                                            <li><Button><Link to="/about">About us</Link></Button></li>
+                                            <li><Button><Link to="/contact">Contact</Link></Button></li>
+                                            <li><Button><Link to="/about">My Account</Link></Button></li>
+                                            <li><Button><Link to="/login">Login</Link></Button></li>
+                                            <li><Button><Link to="/register">Register</Link></Button></li>
+                                            <li><Button><Link to="">Forgot Password</Link></Button></li>
+                                            <li><Button><Link to="">Reset Password</Link></Button></li>
+                                            <li><Button><Link to="">Purchase Guide</Link></Button></li>
+                                            <li><Button><Link to="">Privacy Policy</Link></Button></li>
+                                            <li><Button><Link to="">Term & Services</Link></Button></li>
+                                            <li><Button><Link to="*">404 Page</Link></Button></li>
+                                        
+                                        </ul>
+                                    </div>
+                                </li>
+                                <li className='list-inline-item position-static'>
+                                    <Button><Link to="">Mega Menu <IoIosArrowDown /></Link></Button>
+                                    <div className="dropdown_menu mega_Menu w-100">
+                                        <div className="row">
+                                            <div className="col">
+                                                <h4>Fruits & Vegetables</h4>
+                                            </div>
+                                            <div className="col">
+                                                <h4>Fruits & Vegetables</h4>
+                                            </div>
+                                            <div className="col">
+                                                <h4>Fruits & Vegetables</h4>
+                                            </div>
+                                            <div className="col">
+                                                <img src={Mega} alt="" />
+                                            </div>
+                                        </div>
+                                    
+                                    </div>
                                 </li>
                                 <li className='list-inline-item'>
-                                    <Button><Link  to="/contact">Mega Menu</Link></Button>
+                                    <Button><Link to="">Pages <IoIosArrowDown /></Link></Button>
+                                    <div className="dropdown_menu">
+                                        <ul>
+                                            <li><Button><Link to="/about">About us</Link></Button></li>
+                                            <li><Button><Link to="/contact">Contact</Link></Button></li>
+                                            <li><Button><Link to="/about">My Account</Link></Button></li>
+                                            <li><Button><Link to="/login">Login</Link></Button></li>
+                                            <li><Button><Link to="/register">Register</Link></Button></li>
+                                            <li><Button><Link to="">Forgot Password</Link></Button></li>
+                                            <li><Button><Link to="">Reset Password</Link></Button></li>
+                                            <li><Button><Link to="">Purchase Guide</Link></Button></li>
+                                            <li><Button><Link to="">Privacy Policy</Link></Button></li>
+                                            <li><Button><Link to="">Term & Services</Link></Button></li>
+                                            <li><Button><Link to="*">404 Page</Link></Button></li>
+                                        
+                                        </ul>
+                                    </div>
                                 </li>
                                 <li className='list-inline-item'>
-                                    <Button><Link to="/register">Register</Link></Button>
-                                    <li className='list-inline-item'>
-                                    <Button><Link to="/login">Login</Link></Button>
+                                    <Button><Link to="/contact">Contact</Link></Button>
                                 </li>
-                                </li>
+
                             </ul>
-                            
+
                         </nav>
 
 
 
                     </div>
                     <div className="col-sm-2 part3 d-flex align-items-center">
-                        <div className="phone  d-flex align-items-center">
-                            <span><MdOutlineHeadphones size={30}/></span>
+                        <div className="phone  d-flex align-items-center ml-auto">
+                            <span><MdOutlineHeadphones size={30} /></span>
                             <div className="info ml-3">
                                 <h5 className='mb-0'>021-1234567</h5>
                                 <p className='mb-0'>24/7 Support Center</p>
