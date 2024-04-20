0b9e6b97aacd678a0c53c2410c01d52f0c0f42a5
syeda Faiza <syedafaiza2610@gmail.com>
2024-04-20 22:18:02 +0500

    navbar done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import { FiGrid } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 import Home from '../Pages/Home';
 import { MdOutlineHeadphones } from "react-icons/md";
-import Mega from'../Assets/mega.png'
+import Mega from '../Assets/basket.jpg'
 function Navbar() {
     return (
         <div className='nav d-flex align-items-center'>
@@ -29,18 +29,13 @@ function Navbar() {
                                     <Button><Link to="/shop">Shop</Link></Button>
                                     <div className="dropdown_menu">
                                         <ul>
-                                            <li><Button><Link to="/about">About us</Link></Button></li>
-                                            <li><Button><Link to="/contact">Contact</Link></Button></li>
-                                            <li><Button><Link to="/about">My Account</Link></Button></li>
-                                            <li><Button><Link to="/login">Login</Link></Button></li>
-                                            <li><Button><Link to="/register">Register</Link></Button></li>
-                                            <li><Button><Link to="">Forgot Password</Link></Button></li>
-                                            <li><Button><Link to="">Reset Password</Link></Button></li>
-                                            <li><Button><Link to="">Purchase Guide</Link></Button></li>
-                                            <li><Button><Link to="">Privacy Policy</Link></Button></li>
-                                            <li><Button><Link to="">Term & Services</Link></Button></li>
-                                            <li><Button><Link to="*">404 Page</Link></Button></li>
-                                        
+                                            <li><Button><Link to="">Shop Grid-Right Sidebar</Link></Button></li>
+                                            <li><Button><Link to="">Shop - Wide</Link></Button></li>
+                                            <li><Button><Link to="">Wishlist</Link></Button></li>
+                                            <li><Button><Link to="">Cart</Link></Button></li>
+                                            <li><Button><Link to="">Checkout</Link></Button></li>
+                                            <li><Button><Link to="">Compare</Link></Button></li>
+
                                         </ul>
                                     </div>
                                 </li>
@@ -48,18 +43,10 @@ function Navbar() {
                                     <Button><Link to="/listing">Vendors</Link></Button>
                                     <div className="dropdown_menu">
                                         <ul>
-                                            <li><Button><Link to="/about">About us</Link></Button></li>
-                                            <li><Button><Link to="/contact">Contact</Link></Button></li>
-                                            <li><Button><Link to="/about">My Account</Link></Button></li>
-                                            <li><Button><Link to="/login">Login</Link></Button></li>
-                                            <li><Button><Link to="/register">Register</Link></Button></li>
-                                            <li><Button><Link to="">Forgot Password</Link></Button></li>
-                                            <li><Button><Link to="">Reset Password</Link></Button></li>
-                                            <li><Button><Link to="">Purchase Guide</Link></Button></li>
-                                            <li><Button><Link to="">Privacy Policy</Link></Button></li>
-                                            <li><Button><Link to="">Term & Services</Link></Button></li>
-                                            <li><Button><Link to="*">404 Page</Link></Button></li>
-                                        
+                                            <li><Button><Link to="">Vendor 1</Link></Button></li>
+                                            <li><Button><Link to="">Vendor 2</Link></Button></li>
+                                            <li><Button><Link to="">Vendor 3</Link></Button></li>
+                                            <li><Button><Link to="">Vendor 4</Link></Button></li>
                                         </ul>
                                     </div>
                                 </li>
@@ -68,19 +55,52 @@ function Navbar() {
                                     <div className="dropdown_menu mega_Menu w-100">
                                         <div className="row">
                                             <div className="col">
-                                                <h4>Fruits & Vegetables</h4>
+                                                <h4 className='text-p'>Fruits & Vegetables</h4>
+                                                <ul className="mt-3 mb-0">
+
+                                                    <li><Link>Meat & Poultry</Link></li>
+                                                    <li><Link>Fresh Vegetables</Link></li>
+                                                    <li><Link>Herbs & Seasonings</Link></li>
+                                                    <li><Link>Cuts & Sprouts</Link></li>
+                                                    <li><Link>Exotic Fruits & Veggies</Link></li>
+                                                    <li><Link>Packaged Produce</Link></li>
+
+                                                </ul>
+                                            </div>
+
+
+                                            <div className="col">
+                                                <h4 className='text-p'>Breakfast & Dairy</h4>
+                                                <ul  className="mt-3 mb-0">
+
+                                                    <li><Link>Milk & Flavoured Milk</Link></li>
+                                                    <li><Link>Butter and Margarine</Link></li>
+                                                    <li><Link>Eggs Substitutes</Link></li>
+                                                    <li><Link>Marmalades</Link></li>
+                                                    <li><Link>Sour Cream</Link></li>
+                                                    <li><Link>Cheese</Link></li>
+
+                                                </ul>
                                             </div>
                                             <div className="col">
-                                                <h4>Fruits & Vegetables</h4>
-                                            </div>
-                                            <div className="col">
-                                                <h4>Fruits & Vegetables</h4>
+                                                <h4 className='text-p'>Meat & Seafood</h4>
+
+                                                <ul  className="mt-3 mb-0">
+
+                                                    <li><Link>Breakfast Sausage</Link></li>
+                                                    <li><Link>Dinner Sausage</Link></li>
+                                                    <li><Link>Chicken</Link></li>
+                                                    <li><Link>Sliced Deli Meat</Link></li>
+                                                    <li><Link>Wild Caught Fillets</Link></li>
+                                                    <li><Link>Crab and Shellfish</Link></li>
+
+                                                </ul>
                                             </div>
                                             <div className="col">
                                                 <img src={Mega} alt="" />
                                             </div>
                                         </div>
-                                    
+
                                     </div>
                                 </li>
                                 <li className='list-inline-item'>
@@ -98,7 +118,7 @@ function Navbar() {
                                             <li><Button><Link to="">Privacy Policy</Link></Button></li>
                                             <li><Button><Link to="">Term & Services</Link></Button></li>
                                             <li><Button><Link to="*">404 Page</Link></Button></li>
-                                        
+
                                         </ul>
                                     </div>
                                 </li>
