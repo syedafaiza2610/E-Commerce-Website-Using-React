a2b3abcaa1182ecfeec9134577054820dd3b2587
syeda Faiza <syedafaiza2610@gmail.com>
2024-04-25 01:01:10 +0500

    showing dynamic product in home page

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Lipstick from "../Assets/lipstick.jpg"
 import Rating from '@mui/material/Rating';
 import { Button } from '@mui/material';
@@ -10,64 +10,79 @@ import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
 import Tooltip from '@mui/material/Tooltip';
 
 function Products(props) {
+  // console.log(props.tag)
+
+  const [productData, setproductData] = useState();
+  useEffect(() => {
+    setproductData(props.item)
+  }, [props.item]);
   return (
     <div className='productThumb'>
       {
-        props.tag!==null && props.tag!==undefined &&
+        props.tag!== null && props.tag!== undefined &&
         <span className={`badge1 ${props.tag}`}>{props.tag}</span>
       }
-     
-      <Link>
-        <div className="imgWrapper">
-          <img className='w-100' src={Lipstick} alt="" />
-          <div className="overlay1 transition">
-            <ul className='list list-inline mb-0'>
-              <li className='list-inline-item'>
-              <Tooltip title="Add to Wishlist">
-                <a className='cursor'>
-                  <FavoriteBorderIcon />
-                </a>
-                </Tooltip>
+      {
+        productData !== undefined &&
+        <>
+          <Link>
+            <div className="imgWrapper">
+              <div className='p-4 wrapper'>
+                <img className='w-100' src={productData.catImg + '?im-Resize=(420,420)'} alt="" />
+              </div>
+              <div className="overlay1 transition">
+                <ul className='list list-inline mb-0'>
+                  <li className='list-inline-item'>
+                    <Tooltip title="Add to Wishlist">
+                      <a className='cursor'>
+                        <FavoriteBorderIcon />
+                      </a>
+                    </Tooltip>
 
-              </li>
-              <li className='list-inline-item'>
-              <Tooltip title="Compare">
-                <a className='cursor'>
-                  <CompareArrowsIcon />
-                </a>
-                </Tooltip>
+                  </li>
+                  <li className='list-inline-item'>
+                    <Tooltip title="Compare">
+                      <a className='cursor'>
+                        <CompareArrowsIcon />
+                      </a>
+                    </Tooltip>
 
-              </li>
-              <li className='list-inline-item'>
-              <Tooltip title="Quick View">
-                <a className='cursor'>
-                  <RemoveRedEyeOutlinedIcon />
-                </a>
-                </Tooltip>
-              </li>
-            </ul>
+                  </li>
+                  <li className='list-inline-item'>
+                    <Tooltip title="Quick View">
+                      <a className='cursor'>
+                        <RemoveRedEyeOutlinedIcon />
+                      </a>
+                    </Tooltip>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </Link>
+
+
+
+
+          <div className="info">
+            <span className='d-block catName'>{productData.brand}</span>
+            <h4 className='title'><Link>{productData.productName.substr(0,70)+'...'}</Link> </h4>
+            <Rating name="half-rating-read" defaultValue={parseFloat(productData.rating)} precision={0.5} readOnly />
+            <span className='brand d-block'>By <Link className='clr'>{productData.brand}</Link></span>
+            <div className="d-flex align-items-center ">
+              <div className="d-flex align-items-center">
+                <span className='price'>Rs: {productData.price}</span> <span className='oldprice'>{productData.oldPrice}</span>
+
+              </div>
+              <Button className='addbtn transition'><ShoppingCartOutlinedIcon /> Add</Button>
+
+            </div>
           </div>
-        </div>
-      </Link>
-
-      <div className="info">
-        <span className='d-block catName'>Beauty</span>
-        <h4 className='title'><Link>Longstay Liquid Matte Lipstick</Link> </h4>
-        {/* <Rating name="read-only" value={4} readOnly /> */}
-        <Rating name="half-rating-read" defaultValue={4.5} precision={0.5} readOnly />
-        <span className='brand d-block'>By <Link className='clr'>GoldenRose</Link></span>
-        <div className="d-flex align-items-center ">
-          <div className="d-flex align-items-center">
-            <span className='price'>$28.25</span> <span className='oldprice'>$32.8</span>
-
-          </div>
-          <Button className='addbtn transition'><ShoppingCartOutlinedIcon /> Add</Button>
-
-        </div>
-      </div>
+        </>
+      }
 
 
     </div>
+
   )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,13 +6,13 @@ import Header from '../Components/Header';
 import Category from '../Components/Category';
 import Banner from '../Components/Banner';
 import Products from '../Components/Products';
-import Shampoo from '../Assets/shampo.png'
+import Shampoo from '../Assets/fashion.jpeg'
 import SliderHome from '../Components/Slider';
 import TopProducts from './TopProducts';
 import Newsletter from '../Components/Newsletter';
 import NewsletterPic from '../Assets/news3.png'
 import { Link } from 'react-router-dom';
-import { useState , useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 
 
@@ -23,73 +23,112 @@ function HomeSlider(props) {
   const [activeTab, setactiveTab] = useState();
   const [activeTabIndex, setactiveTabIndex] = useState(0);
   const [activeTabData, setActiveTabData] = useState([]);
+  const [bestSells, setBestSells] = useState([]);
 
   const catArr = [];
 
   useEffect(() => {
     if (productData && productData.length !== 0) {
-        const catArr = [];
-        productData.forEach(item => {
-            if (item.items && item.items.length !== 0) {
-                item.items.forEach(item_ => {
-                    if (item_.cat_name) {
-                        catArr.push(item_.cat_name);
-                    }
-                });
+      const catArr = [];
+      productData.forEach(item => {
+        if (item.items && item.items.length !== 0) {
+          item.items.forEach(item_ => {
+            if (item_.cat_name) {
+              catArr.push(item_.cat_name);
             }
-        });
-        console.log(catArr)
-        const list2 = catArr.filter((item, index) => catArr.indexOf(item) === index);
-        setcatArray(list2);
-        setactiveTab(list2[0]);
+          });
+        }
+      });
+      console.log(catArr)
+      const list2 = catArr.filter((item, index) => catArr.indexOf(item) === index);
+      setcatArray(list2);
+      setactiveTab(list2[0]);
     }
-   
-}, [props.data]);
 
-  
+  }, []);
+  useEffect(() => {
+
+    if (productData && productData.length !== 0) {
+      var arr = [];
+      setActiveTabData([])
+      productData.forEach(item => {
+        if (item.items && item.items.length !== 0) {
+          item.items.forEach(item_ => {
+            if (item_.cat_name === activeTab) {
+              arr.push(...item_.products);
+            }
+          });
+        }
+      });
+      setActiveTabData(arr);
+    }
+  }, [activeTab, productData]);
+  const bestSellsArr = [];
+
+  useEffect(() => {
+      productData.length !== 0 &&
+          productData.map((item) => {
+              if (item.cat_name === "Fashion") {
+                  item.items.length !== 0 &&
+                      item.items.map((item_) => {
+                          item_.products.length !== 0 &&
+                              item_.products.map((product, index) => {
+                                  bestSellsArr.push(product);
+                              })
+                      })
+              }
+
+          });
+
+
+      setBestSells(bestSellsArr);
+
+  }, [])
+
+
+
   var settings = {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     fade: false,
     arrows: true
   };
   return (
     <>
-      <SliderHome/>
+      <SliderHome />
 
-      <Category /> 
+      <Category />
       <Banner />
 
       <section className='homeProducts'>
         <div className="container-fluid">
           <div className="d-flex align-items-center">
             <h2 className='hd mb-0 mt-0'>Popular Products</h2>
-            <ul className=' popular list list-inline ml-auto mb-0'> 
-            {
-                
-                                // catArray.length !== 0 &&
-                                catArray.map((item, index) => {
-                                    return (
-                                        <li className="list list-inline-item">
-                                            <a className={`cursor text-capitalize 
-                                                ${activeTabIndex === index ? 'act' : ''}`}
-                                                onClick={() => {
-                                                    setactiveTab(item)
-                                                    setactiveTabIndex(index);
-                                                    // productRow.current.scrollLeft=0;
-                                                    // setIsLoadingProducts(true);
-                                                }}
-                                            >
-                                                {item}
-                                            </a>
-                                        </li>
-                                    )
-                                })
-                            }
-           
+            <ul className=' popular list list-inline ml-auto mb-0'>
+              {
+
+                catArray.length !== 0 &&
+                catArray.map((item, index) => {
+                  return (
+                    <li className="list list-inline-item">
+                      <a className={`cursor text-capitalize ${activeTabIndex === index ? 'act' : ''}`}
+                        onClick={() => {
+                          // setActiveTabData([])
+                          setactiveTab(item)
+                          setactiveTabIndex(index);
+                         
+                        }}
+                      >
+                        {item}
+                      </a>
+                    </li>
+                  )
+                })
+              }
+
 
             </ul>
           </div>
@@ -97,58 +136,31 @@ function HomeSlider(props) {
 
 
         <div className="productRow">
-          <div className="item">
-            <Products tag="beauty"/>
-          </div>
-          <div className="item">
-            <Products tag ="sale"/>
-          </div>
-          <div className="item">
-            <Products tag="new"/>
-          </div>
-          <div className="item">
-            <Products tag="best"/>
-          </div>
-          <div className="item">
-            <Products/>
-          </div>
-          <div className="item">
-            <Products  tag="sale"/>
-          </div>
-          <div className="item">
-            <Products/>
-          </div>
-          <div className="item">
-            <Products/>
-          </div>
-          <div className="item">
-            <Products  tag="new"/>
-          </div>
-          <div className="item">
-            <Products/>
-          </div>
+          {
+            activeTabData.length !== 0 && 
+            activeTabData.map((item_, index) => { 
+              return (
+                <div className='item' key={index}> 
+                  <Products tag={item_.type} item={item_} /> 
+                </div>
+              );
+            })
+          }
+
+
+      
+
         </div>
-        
+
 
       </section>
-         
-         
+
+
       <section className='homeProducts pt-0 homeProductsRow2'>
         <div className="container-fluid">
           <div className="d-flex align-items-center">
             <h2 className='hd mb-0 mt-0'>Daily Best Sales</h2>
-            <ul className=' popular list list-inline ml-auto mb-0'> 
-            <li className='list-inline-item'>
-              <a className='cursor'>Featured</a>
-            </li>
-            <li className='list-inline-item'>
-              <a className='cursor'>Popular</a>
-            </li>
-            <li className='list-inline-item'>
-              <a className='cursor'>New Added</a>
-            </li>
-
-            </ul>
+          
           </div>
           <br></br>
           <div className="row">
@@ -156,30 +168,21 @@ function HomeSlider(props) {
               <img src={Shampoo} className='w-100' />
             </div>
             <div className="col-md-9">
-            <Slider {...settings} className='prodSlider'>
-              <div className="item">
-              <Products tag="new"/>
-              </div>
-              <div className="item">
-              <Products tag="best"/>
-              </div>
-              <div className="item">
-              <Products tag="new"/>
-              </div>
-              <div className="item">
-              <Products tag="beauty"/>
-              </div>
-              <div className="item">
-              <Products tag="new"/>
-              </div>
-              <div className="item">
-              <Products tag="sale"/>
-              </div>
-              <div className="item">
-              <Products tag="new"/>
-              </div>
-           
-          </Slider>
+              <Slider {...settings} className='prodSlider'>
+                {
+                  
+                    bestSells.length !== 0 &&
+                    bestSells.map((item, index) => {
+                        return (
+                            <div className='item' key={index} style={{minHeight:"600px"}}>
+                                <Products tag="bueaty" item={item} />
+                            </div>
+                        )
+                    })
+                
+                }
+          
+              </Slider>
 
 
             </div>
@@ -187,27 +190,27 @@ function HomeSlider(props) {
         </div>
 
 
-       
+
 
       </section>
       <section className="topProductSection">
         <div className="container-fluid">
           <div className="row">
             <div className="col">
-              <TopProducts title="Top Selling"/>
+              <TopProducts title="Top Selling" />
 
             </div>
             <div className="col">
-            <TopProducts  title="Trending Products"/>
-              
+              <TopProducts title="Trending Products" />
+
             </div>
             <div className="col">
-            <TopProducts  title="Recently Added"/>
-              
+              <TopProducts title="Recently Added" />
+
             </div>
             <div className="col">
-            <TopProducts  title="Top Rated"/>
-              
+              <TopProducts title="Top Rated" />
+
             </div>
           </div>
         </div>
@@ -219,22 +222,22 @@ function HomeSlider(props) {
             <div className="infonews">
               <h2>Stay home & get your daily needs from our shop</h2>
               <p>Start Your daily shopping with <Link>Shopsavvy</Link></p>
-              <Newsletter/>
+              <Newsletter />
 
 
             </div>
             <div className="Imgnews">
               <img src={NewsletterPic} alt="" />
             </div>
-            
-             
+
+
           </div>
         </div>
 
       </section>
       <br></br>
 
-           
+
 
     </>
   )
